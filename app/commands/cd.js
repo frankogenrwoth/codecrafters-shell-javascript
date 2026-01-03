39b1e444df51d73001b2b830fecdf97437c37c4f
frankogenrwoth <121643701+frankogenrwoth@users.noreply.github.com>
2026-01-03 16:33:09 +0300

    fix: correct error message for no directory specified in cd command

--- app/commands/cd.js
+++ app/commands/cd.js
@@ -1,11 +1,25 @@
+const os = require("os");
+
+
 const executeCd = async (args) => {
   return new Promise((resolve) => {
     if (args.length === 0) {
-      // No directory specified, go to home directory
+      console.log("cd: ${args[0]}: No such file or directory");
       resolve({ code: 400 });
     }
 
     const targetDirectory = args[0];
+
+    if (targetDirectory === "~") {
+      try {
+        process.chdir(process.env.HOME || os.homedir());
+        resolve({ code: 200 });
+      } catch (err) {
+        console.log(`cd: ${require("os").homedir()}: No such file or directory`);
+        resolve({ code: 1 });
+      }
+      return;
+    }
 
     try {
       process.chdir(targetDirectory);
