f78d5977e870c922e8531257f0386e4857f9c489
frankogenrwoth <121643701+frankogenrwoth@users.noreply.github.com>
2026-01-03 14:40:03 +0300

    add type command

--- app/main.js
+++ app/main.js
@@ -5,6 +5,40 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+const knownCommands = ["echo", "type", "exit"];
+
+function executeCommand(command, args) {
+  if (!knownCommands.includes(command)) {
+    console.log(`${command}: command not found`);
+    return;
+  }
+
+
+  switch (command) {
+    case "echo":
+      if (args.length === 0) {
+        console.log();
+        return;
+      }
+
+      console.log(args.join(" "));
+      break;
+    case "exit":
+      rl.close();
+      break;
+    case "type":
+      if (args.length === 0) {
+        console.log(`${args[0]}: command not found`);
+      } else {
+        console.log(`${args[0]} is a shell builtin`);
+      }
+      break;
+    default:
+      console.log(`${command}: command not found`);
+  }
+}
+
+
 function inputCommand() {
   rl.question("$ ", (command) => {
     if (command === "exit") {
@@ -13,14 +47,12 @@ function inputCommand() {
     }
     const args = command.split(" ");
     const cmd = args[0];
-
-    if (cmd === "echo") {
-      console.log(args.slice(1).join(" "));
-      inputCommand();
-      return;
+    if (knownCommands.includes(cmd)) {
+      executeCommand(cmd, args.slice(1));
+    } else {
+      console.log(`${command}: command not found`);
     }
 
-    console.log(`${command}: command not found`);
     inputCommand();
   });
 }
