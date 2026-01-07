59f2d1f3032ec06fd2c7586ab7311d7270f6550b
frankogenrwoth <ogenrwothjimfrank@gmail.com>
2026-01-07 16:27:46 +0300

    add escape of back slash

--- app/main.js
+++ app/main.js
@@ -52,9 +52,11 @@ function inputCommand() {
       rl.close();
       return;
     }
-    const source = command.replace(/\\\n/g, "");
+    // const source = command.replace(/\\\n/g, "");
+    const source = command;
     const tokens = await lex(source);
-    const cmd = tokens[0];
+
+    const cmd = String(tokens[0]).replace(/'/g, "\\'");
     await executeCommand(cmd, tokens.slice(1));
 
     // Prompt for the next command after executing the current one
